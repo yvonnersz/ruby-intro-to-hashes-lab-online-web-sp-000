5d42a95bb39c8e0887c5df56219bac9fb9d4cefe
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-30 23:54:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -53,7 +53,7 @@ def monopoly_with_fourth_tier
 				:reading_railroad => {mortgage_value:"$100"},
 				:pennsylvania_railroad => {mortgage_value:"$200"},
 				:b_and_o_railroad => {mortgage_value:"$400"},
-				:shortline => {}
+				:shortline => {mortgage_value:"$800"}
 		},
 		:rent_in_dollars => {
 			one_piece_owned:25,
